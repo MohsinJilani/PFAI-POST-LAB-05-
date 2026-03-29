e7e3cdb6b015632d71c0eccf4e53dbe88ab3c244
MohsinJilani <f24607011@nutech.edu.pk>
2026-03-29 15:46:46 +0500

    First part of code

--- part1.py
+++ part1.py
@@ -1,3 +1,11 @@
 #Person 1 
 import pandas as pd
 import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+#Folder that contains the csv files
+Base_DIR = r"D:\PFAI\PFAILAB05\PFAI-POST-LAB-05-"
+os.path.join(BASE_DIR, "train.csv")
+gender_path = os.path.join(BASE_DIR, "gender_submission.csv")
+
